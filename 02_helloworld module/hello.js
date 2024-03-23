1ab5f1e32ce054b0872ec6dcacdccf7229153f2f
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-03-23 05:52:10 +0530

    first commit

---
+++ 02_helloworld module/hello.js
@@ -0,0 +1,8 @@
+console.log("Hello world! node ");
+const math = require("./math");
+// const fs = require("fs");
+
+console.log(math.add(2, 3));
+console.log(math.sub(2, 3));
+
+// console.log(fs);
---
+++ 02_helloworld module/math.js
@@ -0,0 +1,21 @@
+// *** 1st approach
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = "i am im math";
+// module.exports = { add, sub };
+
+// *** 2nd approach
+
+exports.add = (a, b) => {
+  return a + b;
+};
+exports.sub = (a, b) => {
+  return a - b;
+};
